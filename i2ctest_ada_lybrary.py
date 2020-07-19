a4ab60da941fe92033792ee8bebd2d45aeea23aa
filipesfreitas <filipe.desouzafreitas@gmail.com>
2020-07-19 20:45:08 -0300

    Seguindo a nota de aplicação AN-1057 foi introduzida no arquivo teste a conversão para graus  e os calculos para obtenção das coordenadas esféricas com os ângulos theta e phi.

--- i2ctest_ada_lybrary.py
+++ i2ctest_ada_lybrary.py
@@ -7,15 +7,22 @@ import busio
 import adafruit_adxl34x
 
 
+import math 
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
 
+fator_conv_rad_gra = 180/math.pi
 
 while True:
 
-    print("%f %f %f"%accelerometer.acceleration)
+    txt = "x{0:.2f} m/s\ty{1:.2f} m/s\tz{2:.2f} m/s"
+    acceleration = [i/9.81 if i/9.81<1 and i/9.81>-1 else 0 for i in accelerometer.acceleration]
+    print(acceleration)
+    theta = math.atan(acceleration[1]/acceleration[0])*fator_conv_rad_gra
+    phi = math.acos(acceleration[2]/math.sqrt(pow(acceleration[0],2) + pow(acceleration[1],2) + pow(acceleration[2],2)))*fator_conv_rad_gra
 
+    print(txt.format(accelerometer.acceleration[0],accelerometer.acceleration[1],accelerometer.acceleration[2]),"phi: ",phi,"theta: ",theta)
     time.sleep(1)
