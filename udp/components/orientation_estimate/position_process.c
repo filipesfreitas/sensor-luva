a22d54035a0f153c02146222e71ff522bb304dde
filipesfreitas <filipe.desouzafreitas@gmail.com>
2021-06-16 14:32:05 -0300

    fixed complementary filter commit

---
+++ udp/components/orientation_estimate/position_process.c
@@ -0,0 +1,248 @@
+#include <position_process.h>
+
+void orientation_estimation(raw_data metacarpo,raw_data proximal,Glove* glove,int i) {
+	
+	const float alpha = time_constant/(time_constant+period);
+
+	/* PHI	*/
+	glove->fingers[i].medial.phi = acos(proximal.accelz/(sqrt(pow(proximal.accelx,2)+pow(proximal.accely,2)+
+		pow(proximal.accelz,2))))*(1-alpha) + 
+		(glove->fingers[i].proximal.phi + proximal.gyrox * period ) * alpha ;
+
+	glove->fingers[i].proximal.phi = acos(metacarpo.accelz/(sqrt(pow(metacarpo.accelx,2)+pow(metacarpo.accely,2)+
+		pow(metacarpo.accelz,2))))*(1-alpha) +
+		(glove->fingers[i].medial.phi + metacarpo.gyrox * period ) * alpha ;
+	
+	/* THETA */
+	if (metacarpo.accely==0) glove->fingers[i].proximal.theta = 0;
+	else glove->fingers[i].proximal.theta = atan(metacarpo.accelx/metacarpo.accely)*(1-alpha) + 
+			 alpha * (glove->fingers[i].proximal.theta + metacarpo.gyroz * period);
+
+	if (proximal.accely==0)	glove->fingers[i].medial.theta = 0;
+	else glove->fingers[i].medial.theta = atan(proximal.accelx/proximal.accely)*(1-alpha) +
+			 alpha * (glove->fingers[i].medial.theta + proximal.gyroz * period);
+
+	glove->fingers[i].proximal.phi = glove->fingers[i].proximal.phi - glove -> frame_reference.phi;
+	glove->fingers[i].medial.phi   = glove->fingers[i].medial.phi - glove->fingers[i].proximal.phi - glove -> frame_reference.phi    ;
+
+	glove->fingers[i].proximal.theta = glove -> fingers[i].proximal.theta - glove -> frame_reference.theta ;
+	glove->fingers[i].medial.theta   = glove -> fingers[i].medial.theta - glove -> fingers[i].proximal.theta - glove -> frame_reference.theta;
+
+}
+void reference_frame_orientation(raw_data reference,Glove* glove){
+
+	const float alpha = time_constant/(time_constant+period);
+
+	/* PHI	*/
+	glove->frame_reference.phi = acos(reference.accelz/(sqrt(pow(reference.accelx,2)+
+		pow(reference.accely,2)+pow(reference.accelz,2))))*(1-alpha) + 
+		(glove->frame_reference.phi + reference.gyrox * period ) * alpha * degre_conv;
+
+	/* THETA */
+	if (reference.accely==0) glove->frame_reference.theta = 0;
+	else glove->frame_reference.theta = atan(reference.accelx/reference.accely)*(1-alpha) + 
+			 alpha * (glove->frame_reference.theta + reference.gyroz * period);
+}
+
+void calibration(Glove* glove){
+	int aux = 1;
+	int ret, ret1;
+	int16_t buffer[7];
+	uint8_t sensor[14];
+	uint8_t sensor2[14];
+
+	memset(sensor,0,14);
+	memset(sensor2,0,14);
+	memset(buffer,0,14);
+
+	/* Mux CONFIG*/
+  mux_selector_config();
+  gpio_set_level(pinA, 0);/* Mux initial state*/
+  gpio_set_level(pinB, 1);
+
+	while (aux < 2 ) {
+
+		ret  = i2c_master_read_slave(0, SLAVE1_ADD,START_READ_ADD,sensor, 14);
+		ret1 = i2c_master_read_slave(0, SLAVE2_ADD,START_READ_ADD,sensor2, 14);
+
+		if (ret == ESP_ERR_TIMEOUT || ret1 == ESP_ERR_TIMEOUT) {
+			ESP_LOGE(TAG, "I2C Timeout");
+		} 
+		else if (ret == ESP_OK && ret1 == ESP_OK) {
+			/* Ref*/
+			buffer[0] = (int16_t)((sensor[0]  << 8) | sensor[1]);		/* ACCEL X */
+			buffer[1] = (int16_t)((sensor[2]  << 8) | sensor[3]);		/* ACCEL y */
+			buffer[2] = (int16_t)((sensor[4]  << 8) | sensor[5]);		/* ACCEL z */
+			buffer[3] = (int16_t)((sensor2[0] << 8) | sensor2[1]);	/* ACCEL X */
+			buffer[4] = (int16_t)((sensor2[2] << 8) | sensor2[3]);	/* ACCEL Y */
+			buffer[5] = (int16_t)((sensor2[4] << 8) | sensor2[5]);	/* ACCEL Z */
+
+			/* Pressão através do potenciômetro*/
+			buffer[6] = adc_read(aux,adc_chars);
+			buffer[6] = buffer[6]/(Vinput - buffer[6])*R;
+
+
+			/* PHI	*/
+			glove->fingers[aux].proximal.phi = acos(buffer[2]/(sqrt(pow(buffer[0],2)+pow(buffer[1],2)+pow(buffer[2],2))));
+			glove->fingers[aux].medial.phi = acos(buffer[5]/(sqrt(pow(buffer[3],2)+pow(buffer[4],2)+pow(buffer[5],2))));
+
+			/* THETA */
+			if (buffer[1]==0) glove->fingers[aux].proximal.theta = 0;
+			else glove->fingers[aux].proximal.theta = atan(buffer[0]/buffer[1]);
+
+			if (buffer[4]==0) glove->fingers[aux].medial.theta = 0;
+			else glove->fingers[aux].medial.theta = atan(buffer[3]/buffer[4]);
+			}
+			aux++;
+		}
+}
+
+void initialization(Glove* glove)
+{
+
+	uint8_t addr=1; 
+	/* aux variable for counting*/
+	esp_err_t error_setting[2]={ESP_OK,ESP_OK}; 
+
+	/* Mux CONFIG*/
+  mux_selector_config();
+
+  /* Mux initial state*/
+  gpio_set_level(pinA, 0);
+  gpio_set_level(pinB, 1);
+
+	glove->fingers[0].medial.theta=0;
+	glove->fingers[0].medial.phi=0;
+	glove->fingers[0].proximal.theta=0;
+	glove->fingers[0].proximal.phi=0;
+	glove->fingers[0].pressure=0;
+	glove->fingers[1].medial.theta=0;
+	glove->fingers[1].medial.phi=0;
+	glove->fingers[1].proximal.theta=0;
+	glove->fingers[1].proximal.phi=0;
+	glove->fingers[1].pressure=0;
+	glove->fingers[2].medial.theta=0;
+	glove->fingers[2].medial.phi=0;
+	glove->fingers[2].proximal.theta=0;
+	glove->fingers[2].proximal.phi=0;
+	glove->fingers[2].pressure=0;
+	glove->fingers[3].medial.theta=0;
+	glove->fingers[3].medial.phi=0;
+	glove->fingers[3].proximal.theta=0;
+	glove->fingers[3].proximal.phi=0;
+	glove->fingers[3].pressure=0;
+	glove->fingers[4].proximal.theta=0;
+	glove->fingers[4].proximal.phi=0;
+	glove->frame_reference.theta=0;
+	glove->frame_reference.phi=0;
+	glove->fingers[4].pressure=0;
+	
+	while(addr < 2){
+		gpio_set_level(pinA, (addr & 2) >> 1);
+  	gpio_set_level(pinB, addr & 1);
+		error_setting[0]  = i2c_imu_setup(0,SLAVE1_ADD); 
+		error_setting[1]  = i2c_imu_setup(0,SLAVE2_ADD); 
+
+		if(error_setting[0] || error_setting[1] != ESP_OK){
+			ESP_LOGE(TAG, "Problem at master nº: %d\tNo ack, sensor %s not connected...skip...\n",0,
+			error_setting[0] != ESP_OK ? "SENSOR 0x68" :
+			error_setting[1] != ESP_OK ? "SENSOR 0x69" : "None");
+		}
+		error_setting[0]  = i2c_imu_setup(1,SLAVE1_ADD); 
+		//error_setting[1]  = i2c_imu_setup(1,SLAVE2_ADD); 
+
+		if(error_setting[0] || error_setting[1] != ESP_OK){
+			ESP_LOGE(TAG, "Problem at master nº: %d\tNo ack, sensor %s not connected...skip...\n",1,
+			error_setting[0] != ESP_OK ? "SENSOR 0x68" :
+			error_setting[1] != ESP_OK ? "SENSOR 0x69" : "None");
+		} 	
+  	addr ++;
+
+	}
+	/*
+	error_setting[0]  = i2c_imu_setup(0,SLAVE1_ADD); 
+	error_setting[1]  = i2c_imu_setup(0,SLAVE2_ADD); 
+
+	if(error_setting[0] || error_setting[1] != ESP_OK){
+		ESP_LOGE(TAG, "Problem at master nº: %d\tNo ack, sensor %s not connected...skip...\n",0,
+		error_setting[0] != ESP_OK ? "SENSOR 0x68" :
+		error_setting[1] != ESP_OK ? "SENSOR 0x69" : "None");
+	}
+	
+	gpio_set_level(pinA, 1);
+	gpio_set_level(pinB, 0);
+	error_setting[0]  = i2c_imu_setup(0,SLAVE1_ADD); 
+	error_setting[1]  = i2c_imu_setup(0,SLAVE2_ADD); 
+
+	if(error_setting[0] || error_setting[1] != ESP_OK){
+		ESP_LOGE(TAG, "Problem at master nº: %d\tNo ack, sensor %s not connected...skip...\n",master_num,
+		error_setting[0] != ESP_OK ? "SENSOR 0x68" :
+		error_setting[1] != ESP_OK ? "SENSOR 0x69" : "None");
+	}
+	*/
+}
+
+void buffer_arrange(Glove* glove, char message[])
+{
+
+	sprintf(message,"%.2f|%.2f|%.2f|%.2f|%d|%.2f|%.2f|%.2f|%.2f|%d|%.2f|%.2f|%.2f|%.2f|%d|%.2f|%.2f|%.2f|%.2f|%d|%.2f"
+		"|%.2f|%.2f|%.2f|%d\n",
+	glove->fingers[0].medial.theta*degre_conv,
+	glove->fingers[0].medial.phi*degre_conv,
+	glove->fingers[0].proximal.theta*degre_conv,
+	glove->fingers[0].proximal.phi*degre_conv,
+	glove->fingers[0].pressure,
+	glove->fingers[1].medial.theta*degre_conv,
+	glove->fingers[1].medial.phi*degre_conv,
+	glove->fingers[1].proximal.theta*degre_conv,
+	glove->fingers[1].proximal.phi*degre_conv,
+	glove->fingers[1].pressure,
+	glove->fingers[2].medial.theta*degre_conv,
+	glove->fingers[2].medial.phi*degre_conv,
+	glove->fingers[2].proximal.theta*degre_conv,
+	glove->fingers[2].proximal.phi*degre_conv,
+	glove->fingers[2].pressure,
+	glove->fingers[3].medial.theta*degre_conv,
+	glove->fingers[3].medial.phi*degre_conv,
+	glove->fingers[3].proximal.theta*degre_conv,
+	glove->fingers[3].proximal.phi*degre_conv,
+	glove->fingers[3].pressure,
+	glove->fingers[4].proximal.theta*degre_conv,
+	glove->fingers[4].proximal.phi*degre_conv,
+	glove->frame_reference.theta*degre_conv,
+	glove->frame_reference.phi*degre_conv,
+	glove->fingers[4].pressure);
+}
+
+void raw_data_zero(raw_data* member){
+	
+		member->accelx=0;
+		member->accely=0;
+		member->accelz=0;
+		member->gyrox=0;
+		member->gyroy=0;
+		member->gyroz=0;
+}
+
+void buffer_raw_data(raw_data* member1,raw_data* member2){
+	char message[256];
+	sprintf(message,"%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d\n",
+		member1->master_num,
+		member1->finger,
+		member1->accelx,
+		member1->accely,
+		member1->accelz,
+		member1->gyrox,
+		member1->gyroy,
+		member1->gyroz,
+		member2->master_num,
+		member2->finger,
+		member2->accelx,
+		member2->accely,
+		member2->accelz,
+		member2->gyrox,
+		member2->gyroy,
+		member2->gyroz);
+		ESP_LOGI(TAG,"%s",message);
+
+}
--- udp/main/udp_server.c
+++ udp/main/udp_server.c
@@ -1,12 +1,6 @@
-
-#include "esp_adc_cal.h"
 #include "esp_event.h"
 #include "esp_log.h"
-#include "esp_system.h"
 #include "esp_wifi.h"
-#include "driver/adc.h"
-#include "driver/gpio.h"
-#include "driver/i2c.h"
 #include "freertos/event_groups.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -20,284 +14,150 @@
 #include "position_process.h"
 #include <string.h>
 #include <sys/param.h>
+#include <math.h>
+#include <unistd.h>
+#include <esp_system.h>
+#include <time.h>
+#include <sys/time.h>
+#include "esp_timer.h"
+#include <misc.h>
+#include <position_process.h>
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include <i2c_handler.h>
 
 #define PORT CONFIG_EXAMPLE_PORT
 
-#define I2C_MASTER_FREQ_HZ 400000        /* I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE 0      /* I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE 0      /* I2C master doesn't need buffer */
-#define SDA1 18
-#define SCL1 19
-#define SDA2 22 
-#define SCL2 23
-#define WRITE_BIT               I2C_MASTER_WRITE /*!< I2C master write */
-#define READ_BIT                I2C_MASTER_READ  /*!< I2C master read */
-#define ACK_CHECK_EN            0x1              /*!< I2C master will check ack from slave*/
-#define ACK_CHECK_DIS           0x0              /*!< I2C master will not check ack from slave */
-#define ACK_VAL                 0x0              /*!< I2C ack value */
-#define NACK_VAL                0x1              /*!< I2C nack value */
-#define LAST_NACK_VAL           0x2
+/* Event group bit set*/
+#define PORT0ADX  	( 1UL << 0UL )  /* Event bit 0, read two sensors at the port0 i2c */
+#define PORT1ADX  	( 1UL << 1UL ) 	/* Event bit 1, read two sensors at the port1 i2c */
+#define UDP  	  		( 1UL << 2UL )  /* Event bit 2, UDP server */
+#define DISPBUFFER 	( 1UL << 3UL )  /* Display buffer for channel reading*/
+#define SYNC        ( 1UL << 4UL )  /* Flag Sync, change the channel of the mux*/
+#define PRINT 		PORT0ADX  
+#define ALLSYNCH  DISPBUFFER | PRINT | SYNC /* check all samples were readed*/
 
-#define SLAVE1_ADD 0x68
-#define SLAVE2_ADD 0x69
-#define CTRL1_XL 0x10
-#define CTRL2_G  0x11
-#define CTRL9_XL 0X18
-#define CTRL10_C 0X19
-#define CTRL6_G 0X15
-#define CTRL10_G 0X
-#define ENABLE_ACC_GIRO 0x38
-#define CONFIG 0x60
-#define START_READ_ADD 0x20
 
-#define PORT0ADX  ( 1UL << 0UL )  /* Event bit 0, read two sensors at the port0 i2c */
-#define PORT1ADX 	( 1UL << 1UL ) 	/* Event bit 1, read two sensors at the port1 i2c */
-#define UDP  			( 1UL << 2UL )  /* Event bit 2, UDP server */
-#define ALLSYNCH   PORT0ADX | UDP /* check all samples were readed*/
-#define SYNCHDATA PORT1ADX 
-
-#define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
-#define NO_OF_SAMPLES   64          //Multisampling
-
-static const char *TAG = "TAG_ESP";
-
+/* Event group instantiation*/
 EventGroupHandle_t xEventGroup;
 
 QueueHandle_t buffer_queue; 
 
-static esp_adc_cal_characteristics_t *adc_chars;
-static const adc_channel_t channel = ADC_CHANNEL_0;     //GPIO 36
-static const adc_atten_t atten = ADC_ATTEN_DB_11;
-static const adc_unit_t unit = ADC_UNIT_1;
-
-
-static esp_err_t i2c_master_read_slave(i2c_port_t i2c_num,uint8_t device ,uint8_t reg_address, uint8_t *data, size_t data_len)
-{
-	/* Master read from slave on I2C BUS.
-	@param
-	*/
-	int ret;
-	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-	i2c_master_start(cmd);
-	i2c_master_write_byte(cmd, device << 1 | WRITE_BIT, ACK_CHECK_EN);
-	i2c_master_write_byte(cmd, reg_address, ACK_CHECK_EN);
-	i2c_master_stop(cmd);
-	ret = i2c_master_cmd_begin(i2c_num, cmd, 10 / portTICK_RATE_MS);
-	i2c_cmd_link_delete(cmd);
-
-	if (ret != ESP_OK) {
-		return ret;
-	}
-
-	cmd = i2c_cmd_link_create();
-	i2c_master_start(cmd);
-	i2c_master_write_byte(cmd, device << 1 | READ_BIT, ACK_CHECK_EN);
-	i2c_master_read(cmd, data, data_len, LAST_NACK_VAL);
-	i2c_master_stop(cmd);
-	ret = i2c_master_cmd_begin(i2c_num, cmd, 10 / portTICK_RATE_MS);
-	i2c_cmd_link_delete(cmd);
-
-	return ret;
-}
-
-static esp_err_t i2c_master_write_slave(i2c_port_t i2c_num ,uint8_t device ,uint8_t reg_address, uint8_t *data, size_t data_len)
-{
-	int ret;
-	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-	i2c_master_start(cmd);
-	i2c_master_write_byte(cmd, device << 1 | WRITE_BIT, ACK_CHECK_EN);
-	i2c_master_write_byte(cmd, reg_address, ACK_CHECK_EN);
-	i2c_master_write(cmd, data, data_len, ACK_CHECK_EN);
-	i2c_master_stop(cmd);
-	ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
-	i2c_cmd_link_delete(cmd);
-
-	return ret;
-}
-
-static esp_err_t i2c_imu_setup(i2c_port_t MASTER_NUMBER,uint8_t device)
-{
-	uint8_t cmd_data;
-	vTaskDelay(100 / portTICK_RATE_MS);
-
-	cmd_data = CONFIG;    // Acc and Gyro 416Hz
-	ESP_ERROR_CHECK(i2c_master_write_slave(MASTER_NUMBER, device,CTRL1_XL, &cmd_data, 1));
-	ESP_ERROR_CHECK(i2c_master_write_slave(MASTER_NUMBER, device,CTRL2_G, &cmd_data, 1));
-
-
-	cmd_data = ENABLE_ACC_GIRO;    // ENABLE ACCEL AND GYRO
-	ESP_ERROR_CHECK(i2c_master_write_slave(MASTER_NUMBER, device,CTRL9_XL, &cmd_data, 1));
-	ESP_ERROR_CHECK(i2c_master_write_slave(MASTER_NUMBER, device,CTRL10_C, &cmd_data, 1));
-
-
-	return ESP_OK;
-}
-
-static esp_err_t i2c_master_init(i2c_port_t MASTER_NUMBER, int sda, int scl)
-{
-
-	i2c_config_t conf;
-	conf.mode = I2C_MODE_MASTER;
-	conf.sda_io_num = sda;
-	conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-	conf.scl_io_num = scl;
-	conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-	conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
-	i2c_param_config(MASTER_NUMBER, &conf);
-	return i2c_driver_install(MASTER_NUMBER, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
-}
-
+/*Glove instance*/
+Glove* glove;
 
 static void disp_buf(void * pvParameters)
 {
-	int len_to_send=0;
-	int16_t tx_buffer[7];
+
 	char tx_buffer_msg[256]={'0'};
-	int i=1;
 	while(1){
-		xEventGroupWaitBits(xEventGroup, PORT0ADX, pdFALSE,pdTRUE,(TickType_t)1);
-		i =1;
-		do{
-			if(xQueueReceive(buffer_queue, &tx_buffer, portMAX_DELAY))
-			{
-				len_to_send = sprintf(tx_buffer_msg,"%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d\r\n",
-					tx_buffer[0],
-					tx_buffer[1],
-					tx_buffer[2],
-					tx_buffer[3],
-					tx_buffer[4],
-					tx_buffer[5],
-					tx_buffer[6],
-					tx_buffer[7],
-					tx_buffer[8],
-					tx_buffer[9],
-					tx_buffer[10],
-					tx_buffer[11],
-					tx_buffer[12],
-					tx_buffer[13],
-					tx_buffer[14]);
-
-				ESP_LOGI(TAG,"%s\n",tx_buffer_msg);
-			}
-
-		}while(i++ == 2);
-		xEventGroupSetBits(xEventGroup,(EventBits_t) pvParameters);
+		xEventGroupSync(xEventGroup,DISPBUFFER,PRINT,portMAX_DELAY);
+		buffer_arrange(glove, tx_buffer_msg);
+		ESP_LOGI(TAG,"%s",tx_buffer_msg);
 	}
 }
 
-
-static void check_efuse(void)
+/* Task for reading I2C data on master 0*/
+static void i2c_task0(void *pvParameters)
 {
-    //Check TP is burned into eFuse
-    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
-        printf("eFuse Two Point: Supported\n");
-    } else {
-        printf("eFuse Two Point: NOT supported\n");
-    }
-
-    //Check Vref is burned into eFuse
-    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
-        printf("eFuse Vref: Supported\n");
-    } else {
-        printf("eFuse Vref: NOT supported\n");
-    }
-}
-static void print_char_val_type(esp_adc_cal_value_t val_type)
-{
-    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
-        printf("Characterized using Two Point Value\n");
-    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
-        printf("Characterized using eFuse Vref\n");
-    } else {
-        printf("Characterized using Default Vref\n");
-    }
-}
-
-int16_t adc_read(){
-	//Check if Two Point or Vref are burned into eFuse
-	check_efuse();
-	//Configure ADC
-  adc1_config_width(ADC_WIDTH_BIT_12);
-  adc1_config_channel_atten(channel, atten);		
-
-  adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
-  esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
-  print_char_val_type(val_type);
-	//Continuously sample ADC1
-
-		uint32_t adc_reading = 0;
-		//Multisampling
-		for (int i = 0; i < NO_OF_SAMPLES; i++) {
-			adc_reading += adc1_get_raw((adc1_channel_t)channel);
-		}
-		adc_reading /= NO_OF_SAMPLES;
-
-		//Convert adc_reading to voltage in mV
-		int16_t voltage = (int16_t) esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
-		return voltage;	
-}
-
-static void i2c_task(void *pvParameters)
-{
+	/* IMU CONFIG*/
+	int finger=1;
 	int ret, ret1;
-	int16_t buffer[15];
+	raw_data metacarpo, proximal;
 	uint8_t sensor[14];
 	uint8_t sensor2[14];
 
-	i2c_port_t master_num=0;
-	esp_err_t error_setting[2]={ESP_OK,ESP_OK}; 
-
+	int16_t buffer;
+	i2c_port_t master_num=(int) pvParameters;
+        	
 	memset(sensor,0,14);
 	memset(sensor2,0,14);
-	memset(buffer,0,30);
+	raw_data_zero(&metacarpo);
+	raw_data_zero(&proximal);
 
-	i2c_master_init(master_num,SDA1,SCL1);
+	while (1) {	
+		if (finger > 1) finger = 1;
 
-	error_setting[0] =  i2c_imu_setup(master_num,SLAVE1_ADD); 
-	error_setting[1]  = i2c_imu_setup(master_num,SLAVE2_ADD); 
-
-	if(error_setting[0] || error_setting[1] != ESP_OK){
-		ESP_LOGE(TAG, "Problem at master nº: %d\tNo ack, sensor %s not connected...skip...\n",master_num,
-		error_setting[0] == ESP_OK ? "SENSOR 0x68" :
-		error_setting[1] != ESP_OK ? "SENSOR 0x69" : "None");
-	}
-	while (1) {
-
-		ret = i2c_master_read_slave(master_num, SLAVE1_ADD,START_READ_ADD,sensor, 14);
+		ret  = i2c_master_read_slave(master_num, SLAVE1_ADD,START_READ_ADD,sensor, 14);
 		ret1 = i2c_master_read_slave(master_num, SLAVE2_ADD,START_READ_ADD,sensor2, 14);
+
 		if (ret == ESP_ERR_TIMEOUT || ret1 == ESP_ERR_TIMEOUT) {
-			ESP_LOGE(TAG, "I2C Timeout");
+			ESP_LOGW(TAG,"\vProblem at master nº: %d\tSensor %s timed out...skip...\v",master_num,
+			ret  != ESP_OK ? "SENSOR 0x68" :
+			ret1 != ESP_OK ? "SENSOR 0x69" : "None");
 		} 
 		else if (ret == ESP_OK && ret1 == ESP_OK) {
 			/* Ref*/
-			buffer[0]  = (int16_t)((sensor[1]  << 8)   | sensor[0]);	    /* TEMP	  */
-			buffer[1]  = (int16_t)((sensor[3]  << 8)   | sensor[2]);		  /* GIRO X */
-			buffer[2]  = (int16_t)((sensor[5]  << 8)   | sensor[4]);		  /* GIRO Y */
-			buffer[3]  = (int16_t)((sensor[7]  << 8)   | sensor[6]);		  /* GIRO Z */
-			buffer[4]  = (int16_t)((sensor[9]  << 8)   | sensor[8]);		  /* ACCEL X*/
-			buffer[5]  = (int16_t)((sensor[11] << 8)   | sensor[10]);	    /* ACCEL Y*/
-			buffer[6]  = (int16_t)((sensor[13] << 8)   | sensor[12]);	    /* ACCEL Z*/
+			metacarpo.finger	= finger;
+			metacarpo.finger	= master_num;
+			metacarpo.accelx	= (int16_t)((sensor[0]   << 8) | sensor[1]);	/* ACCEL X */
+			metacarpo.accely	= (int16_t)((sensor[2]   << 8) | sensor[3]);	/* ACCEL y */
+			metacarpo.accelz	= (int16_t)((sensor[4]   << 8) | sensor[5]);	/* ACCEL z */
+			metacarpo.gyrox		= (int16_t)((sensor[8]   << 8) | sensor[9]);	/* GIRO X  */
+			metacarpo.gyroy		= (int16_t)((sensor[10]  << 8) | sensor[11]); /* GIRO Y  */
+			metacarpo.gyroz		= (int16_t)((sensor[12]  << 8) | sensor[13]); /* GIRO Z  */
+			proximal.accelx		= (int16_t)((sensor2[0]  << 8) | sensor2[1]); /* ACCEL X */
+			proximal.accely		= (int16_t)((sensor2[2]  << 8) | sensor2[3]);	/* ACCEL Y */
+			proximal.accelz		= (int16_t)((sensor2[4]  << 8) | sensor2[5]);	/* ACCEL Z */
+			proximal.gyrox		= (int16_t)((sensor2[8]  << 8) | sensor2[9]);	/* GIRO X  */
+			proximal.gyroy		= (int16_t)((sensor2[10] << 8) | sensor2[11]);/* GIRO Y  */
+			proximal.gyroz		= (int16_t)((sensor2[12] << 8) | sensor2[13]);/* GIRO Z  */
+			
+			/* Pressão através do potenciômetro*/
+			buffer = adc_read(finger,adc_chars);
+			glove -> fingers[finger].pressure = buffer/(Vinput - buffer)*R;
+			orientation_estimation(metacarpo,proximal,glove,finger);
+			//buffer_raw_data(&metacarpo,&proximal);
 
-			buffer[7]  = (int16_t)((sensor2[1]  << 8)  | sensor2[0]);		  /* TEMP	  */
-			buffer[8]  = (int16_t)((sensor2[3]  << 8)  | sensor2[2]);		  /* GIRO X */
-			buffer[9]  = (int16_t)((sensor2[5]  << 8)  | sensor2[4]);		  /* GIRO Y */
-			buffer[10] = (int16_t)((sensor2[7]  << 8)  | sensor2[6]);		  /* GIRO Z */
-			buffer[11] = (int16_t)((sensor2[9]  << 8)  | sensor2[8]);		  /* ACCEL X*/
-			buffer[12] = (int16_t)((sensor2[11] << 8)  | sensor2[10]);	  /* ACCEL Y*/
-			buffer[13] = (int16_t)((sensor2[13] << 8)  | sensor2[12]);	  /* ACCEL Z*/
-			/* Angulo através do potenciômetro*/
-			buffer[14] = adc_read();
+		}
+	memset(sensor,0,14);
+	memset(sensor2,0,14);
+	raw_data_zero(&metacarpo);
+	raw_data_zero(&proximal);
+	finger++;
 
-			if(xQueueSend(buffer_queue, buffer, portMAX_DELAY)!=pdPASS){
-				ESP_LOGE(TAG, "failed to post on queue");
-			}
-		} 
-		xEventGroupSync(xEventGroup,PORT0ADX,ALLSYNCH,portMAX_DELAY);
-		memset(sensor,0,14);
-		memset(sensor2,0,14);
-		memset(buffer,0,30);
+	xEventGroupSync(xEventGroup,PORT0ADX,DISPBUFFER,portMAX_DELAY);
 	}
 	vTaskDelete(NULL);
+}
+
+static void i2c_task_reference_frame(void *pvParameters)
+{
+	
+	int ret;
+	raw_data ref;
+	uint8_t sensor[14];
+
+	i2c_port_t master_num = (int) pvParameters;
+        	
+	memset(sensor,0,14);
+	raw_data_zero(&ref);
+
+	while (1) {	
+
+		ret  = i2c_master_read_slave(master_num, SLAVE1_ADD,START_READ_ADD,sensor, 14);
+
+		if (ret == ESP_ERR_TIMEOUT) {
+			ESP_LOGE(TAG, "I2C Timeout");
+		} 
+		else if (ret == ESP_OK) {
+			/* Ref*/
+			ref.master_num  = master_num;
+			ref.finger  = 0;
+			ref.accelx  = (int16_t)((sensor[0]   << 8)  | sensor[1]); /* ACCEL X */
+			ref.accely  = (int16_t)((sensor[2]   << 8)  | sensor[3]); /* ACCEL y */
+			ref.accelz  = (int16_t)((sensor[4]   << 8)  | sensor[5]); /* ACCEL z */
+			ref.gyrox   = (int16_t)((sensor[8]   << 8)  | sensor[9]); /* GIRO X  */
+			ref.gyroy   = (int16_t)((sensor[10]  << 8)  | sensor[11]);/* GIRO Y  */
+			ref.gyroz   = (int16_t)((sensor[12]  << 8)  | sensor[13]);/* GIRO Z  */
+			
+			reference_frame_orientation(ref,glove);
+		}
+		memset(sensor,0,14);
+		raw_data_zero(&ref);
+		xEventGroupSync(xEventGroup,PORT1ADX,DISPBUFFER,portMAX_DELAY);
+	}
 }
 
 /*
@@ -314,10 +174,9 @@ static void udp_server_task(void *pvParameters)
 	struct sockaddr_in6 dest_addr;
 
 	char rx_buffer[128]={'0'};
-	int len_to_send=0;
+	char tx_buffer_msg[512]={'0'};
+
 	int16_t tx_buffer[15];
-	char tx_buffer_msg[256]={'0'};
-	int i;
 
 	struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)&dest_addr;
 	dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
@@ -366,35 +225,14 @@ static void udp_server_task(void *pvParameters)
 	ESP_LOGI(TAG, "%s", rx_buffer);
 
 	while(1){
-		xEventGroupWaitBits(xEventGroup, PORT0ADX | PORT1ADX, pdFALSE,pdTRUE,(TickType_t)1);
-		i =1;
-		do{
-			if(xQueueReceive(buffer_queue, &tx_buffer, pdMS_TO_TICKS(10))==true){
-				len_to_send = sprintf(tx_buffer_msg,"%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d\r\n",
-					tx_buffer[0],
-					tx_buffer[1],
-					tx_buffer[2],
-					tx_buffer[3],
-					tx_buffer[4],
-					tx_buffer[5],
-					tx_buffer[6],
-					tx_buffer[7],
-					tx_buffer[8],
-					tx_buffer[9],
-					tx_buffer[10],
-					tx_buffer[11],
-					tx_buffer[12],
-					tx_buffer[13],
-					tx_buffer[14]);
 
-				int err = sendto(sock, tx_buffer_msg, len_to_send, 0, (struct sockaddr *)&source_addr, sizeof(source_addr));
+		xEventGroupWaitBits(xEventGroup, ALLSYNCH , pdFALSE,pdTRUE,(TickType_t)1);
+		
+		if(xQueueReceive(buffer_queue, &tx_buffer, pdMS_TO_TICKS(10))==true){
 
-				if (err < 0) {
-					ESP_LOGE(TAG, "Error occured during sending: errno %x\t%s", errno,esp_err_to_name_r(errno,error_code_,512));
-					break;
-				}
-			}
-		}while(i++ == 2);
+				buffer_arrange(glove,tx_buffer_msg);
+				int err = sendto(sock, tx_buffer_msg, strlen(tx_buffer_msg), 0, (struct sockaddr *)&source_addr, sizeof(source_addr));
+
 		if (err<0)
 		{
 			break;
@@ -410,22 +248,60 @@ if (sock != -1) {
 }
 }
 vTaskDelete(NULL);  
-} 
+}} 
 
+/**/
+static void sync_task(void *pvParameters)
+{
+	uint8_t addr=1; /* aux variable for counting*/
+ 	const TickType_t xDelay = 50 / portTICK_PERIOD_MS;/*delay de 50 ms*/
 
+	/* Mux CONFIG*/
+  mux_selector_config();
+	while(1){
+		gpio_set_level(pinA, 0);
+		gpio_set_level(pinB, 1);
+
+		while(addr < 1) { 		/* Check if addr overflow*/
+			xEventGroupSync(xEventGroup,SYNC,PRINT,portMAX_DELAY);
+			addr ++;
+  		gpio_set_level(pinA, (addr & 2) >> 1);
+  		gpio_set_level(pinB, addr & 1);
+  	}
+  	//addr = 0;
+  	xEventGroupWaitBits(xEventGroup, DISPBUFFER , pdFALSE,pdTRUE,(TickType_t)1);
+  	vTaskDelay(xDelay);
+
+	}
+	vTaskDelete(NULL);  
+}
+
+/* Main function*/
 void app_main(void)
 {
-
 	xEventGroup = xEventGroupCreate();
 
-	buffer_queue = xQueueCreate(6, 15*sizeof(int16_t));//Cria a queue *buffer* com 6 slots de 28 bytes
+	buffer_queue = xQueueCreate(6, sizeof(Glove));//Create buffer queue with 6 slots of glove structure
+	glove = (Glove *)malloc(sizeof(Glove));
+	
+	/*
 	ESP_ERROR_CHECK(nvs_flash_init());
 	ESP_ERROR_CHECK(esp_netif_init());
 	ESP_ERROR_CHECK(esp_event_loop_create_default());
-
 	ESP_ERROR_CHECK(example_connect());
+	*/
+  adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
 
-	xTaskCreate(udp_server_task, "udp_server_task", 4096, (void*)AF_INET, 5, NULL);//!Task instance for udp comunication
-	xTaskCreate(i2c_task  , "i2c_test_task_0", 2048, (void *)PORT0ADX, 20, NULL); //!Task instance for I2C BUS read.
-	//xTaskCreate(disp_buf  , "disp_buf", 2048, (void *)UDP, 20, NULL);//!< Task instance for prety print I2C BUS on esp32 monitor on PC.
+	i2c_master_init(MASTER_0,SDA1,SCL1);
+	i2c_master_init(MASTER_1,SDA2,SCL2);
+
+	initialization(glove);
+	calibration(glove);
+	adc_config();	
+	
+	//xTaskCreate(udp_server_task, "udp_server_task", 4096, (void*)AF_INET, 5, NULL);//!Task instance for udp comunication
+	xTaskCreate(sync_task , "sync_task", 2048, (void *)0, 20, NULL); //!Task instance for I2C BUS read.
+	xTaskCreate(i2c_task0 , "i2c_test_task_0", 4096, (void *)0, 20, NULL); //!Task instance for I2C BUS read.
+	xTaskCreate(i2c_task_reference_frame , "i2c_task_reference_frame", 2048, (void *)1, 20, NULL); //!Task instance for I2C BUS read.
+	xTaskCreate(disp_buf  , "disp_buf", 4096, (void *)UDP, 20, NULL);//!< Task instance for prety print I2C BUS on esp32 monitor on PC.
 }
