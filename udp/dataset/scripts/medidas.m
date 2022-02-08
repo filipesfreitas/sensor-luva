aa691bba895048798a3b7d4852316c6cf1b5322f
filipesfreitas <filipe.desouzafreitas@gmail.com>
2022-02-07 22:27:12 -0300

    regretion script added

---
+++ udp/dataset/scripts/medidas.m
@@ -0,0 +1,8 @@
+function [ mean_f ,std_f ] = medidas(variable)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+std_f = std(variable);
+mean_f = mean(variable);
+
+end
+
